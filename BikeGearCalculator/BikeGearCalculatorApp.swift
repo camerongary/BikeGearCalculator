b61ffd247d6710573ca4f3d29d3b4d76c52f319d
camerongary <97324299+camerongary@users.noreply.github.com>
2026-05-17 12:24:11 -0700

    Initial Commit

---
+++ BikeGearCalculator/BikeGearCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BikeGearCalculatorApp.swift
+//  BikeGearCalculator
+//
+//  Created by Cameron Gary on 5/17/26.
+//
+
+import SwiftUI
+
+@main
+struct BikeGearCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
